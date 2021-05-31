6f8b6582468e533eccf9a73f61e2d965e5481140
RiomhMarsh <marsh.riomh@gmail.com>
2021-05-31 19:27:33 +1200

    Added Contact Info Function

--- 103App_Riomh+Liam/103App_Riomh+Liam.cpp
+++ 103App_Riomh+Liam/103App_Riomh+Liam.cpp
@@ -13,13 +13,26 @@ using namespace std;
 
 //CONTACT FUNCTION
 //****************
-//this is also a test-Riomh
+void display_contact_info()
+{
+    system("CLS");
+    cout << "\n\t\t\t\t\tCONTACT INFO\n";
+    cout << "********************************************************************************************\n\n";
+    cout << "\tPhone:\t\t0800 448 325\n";
+    cout << "\tFacebook:\tfacebook.com/NewZealandBloodService\n";
+    cout << "\tYouTube:\tyoutube.com/user/savelivesgiveblood\n";
+    cout << "\tInstagram:\tinstagram.com/nzbloodservice\n";
+    cout << "\tLinkedIn:\tlinkedin.com/company/new-zealand-blood-service\n\n";
+    cout << "********************************************************************************************\n\n";
+    system("PAUSE");
+    system("CLS");
+}
 //****************
 
 int main()
 {
     //INTRO MENU
     //****************
-
+    
     //****************
 }
