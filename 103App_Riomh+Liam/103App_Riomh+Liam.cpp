32c3c146545dea671fda349783cab201aaec0658
Liam Johns <liamjohns.rainbow@gmail.com>
2021-06-03 16:52:56 +1200

    info has been added ill add more later

--- 103App_Riomh+Liam/103App_Riomh+Liam.cpp
+++ 103App_Riomh+Liam/103App_Riomh+Liam.cpp
@@ -8,7 +8,13 @@ using namespace std;
 
 //INFO FUNCTION
 //****************
+void intro_function() {
+    cout << "\nWho are we?" << endl;
+    cout << "Capital Blood Bank is a New Zealand based bloodbank with locations across the north and south island.";
+    cout << "As a bloodbank we ask that those who are able to donate blood to donate it as many peole in New Zealand could use that blood for opperations and other blood repuired practices";
+    cout << "If you are interested in donating blood or are in need of blood please register and visit the contact tab for furthur information";
 
+}
 //****************
 
 //CONTACT FUNCTION
